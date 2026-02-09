0fd92a8ebf5a757e2b437307beaa88d2906a1e33
DiazWilcris <wilcrisdiazv04@gmail.com>
2026-02-09 19:59:31 -0400

    First Fase

---
+++ P2_Assignmentss/StarAtlas.API/Controllers/BodyTypesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarAtlas.API.Data;
+using StarAtlas.API.Models.Entities;
+using StarAtlas.API.Models.Dtos;
+
+namespace StarAtlas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BodyTypesController : ControllerBase
+    {
+        private readonly StarAtlasContext _context;
+
+        public BodyTypesController(StarAtlasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BodyTypeDto>>> GetBodyTypes()
+        {
+            var types = await _context.BodyTypes.ToListAsync();
+
+            var dtos = types.Select(t => new BodyTypeDto
+            {
+                Id = t.Id,
+                Name = t.Name
+            }).ToList();
+
+            return Ok(dtos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BodyType>> PostBodyType(BodyType bodyType)
+        {
+            _context.BodyTypes.Add(bodyType);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetBodyTypes", new { id = bodyType.Id }, bodyType);
+        }
+    }
+}
---
+++ P2_Assignmentss/StarAtlas.API/Data/StarAtlasContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarAtlas.API.Models.Entities; 
+
+namespace StarAtlas.API.Data
+{
+    public class StarAtlasContext : DbContext
+    {
+        public StarAtlasContext(DbContextOptions<StarAtlasContext> options) : base(options) { }
+
+        public DbSet<CelestialBody> CelestialBodies { get; set; }
+        public DbSet<BodyType> BodyTypes { get; set; }
+        public DbSet<Observation> Observations { get; set; }
+    }
+}
