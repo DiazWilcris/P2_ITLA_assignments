8b1edaa0f6b112ed70b60bf825b1e42df17d02d7
DiazWilcris <wilcrisdiazv04@gmail.com>
2026-02-09 20:27:48 -0400

    .

---
+++ P2_Assignments/src/StarAtlas.API/Models/Dtos/CreateObservationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarAtlas.API.Models.Dtos
+{
+    public class CreateObservationDto
+    {
+        [Required]
+        public int CelestialBodyId { get; set; }
+
+        [Required]
+        public string PersonalNote { get; set; } = string.Empty;
+
+        public string? Location { get; set; }
+    }
+}
